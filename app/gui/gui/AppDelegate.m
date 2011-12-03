30af2962143c50402b1aedc51111e2f57f4394a5
volodg <volodg@mail.ru>
2011-12-03 15:29:04 +0200

    first commit

---
+++ app/gui/gui/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  gui
+//
+//  Created by Vladimir on 28.11.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+   // Insert code here to initialize your application
+}
+
+@end
