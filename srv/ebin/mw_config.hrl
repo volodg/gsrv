6bf305cfdc607243421089fd9502e025e34e6064
volodg <volodg@mail.ru>
2011-12-06 00:31:52 +0200

    first commit

---
+++ srv/ebin/mw_config.hrl
@@ -0,0 +1,4 @@
+
+%Имя ноды на которой запускается сервер
+-define(SRV_NODE, 'server@Mac-Pro-Vladimir').
+%-define(SRV_NODE, 'server@vgor').
---
+++ srv/ebin/mw_start.erl
@@ -0,0 +1,129 @@
+-module(mw_start).
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0,
+		stop/0,
+		play_battleground/1]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-include("mw_config.hrl").
+
+-define(SERVER, ?MODULE).
+
+%%====================================================================
+%% API
+%%====================================================================
+start_link() ->
+	gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+stop() ->
+	case whereis(?SERVER) of
+		undefined ->
+			io:fwrite( "Server not runned~n" );
+		Pid ->
+			exit( Pid, normal )
+	end.
+
+%%--------------------------------------------------------------------
+%% Function: notify(Msg) -> ok
+%% Description: Creates a bank account for the person with name Name
+%%--------------------------------------------------------------------
+play_battleground( Cid ) ->
+	%validate arguments here, in client thread !!!
+	gen_server:call( { ?SERVER, ?SRV_NODE }, {play_battleground, Cid}).
+
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% Function: init(Args) -> {ok, State} |
+%%                         {ok, State, Timeout} |
+%%                         ignore               |
+%%                         {stop, Reason}
+%% Description: Initiates the server
+%%--------------------------------------------------------------------
+init([]) ->
+%	process_flag(trap_exit, true),
+% STODO precess killed PendingPlayer
+%	io:fwrite( "Init with instruments: ~p~n", [?INSTRUMENTS] ),
+	State = dict:store( pending_player, nil, dict:new() ),
+	{ ok, State }.
+
+%%--------------------------------------------------------------------
+%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
+%%                                      {reply, Reply, State, Timeout} |
+%%                                      {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, Reply, State} |
+%%                                      {stop, Reason, State}
+%% Description: Handling call messages
+%%--------------------------------------------------------------------
+handle_call({play_battleground, Cid}, From, State) ->
+	io:fwrite( "HERE XXX 20.0 ~n", [] ),
+	case get_pending_player( State ) of
+		nil ->
+			NewState = set_pending_player( State, { Cid, From } ),
+			{reply, wait, NewState};
+		{ _PendingPlayerCid, PendingPlayerPid } ->
+			%STODO create game
+			io:fwrite( "HERE 21.0 ~n", [] ),
+			NewState2 = set_pending_player( State, nil ),
+			io:fwrite( "HERE 22.0 ~n", [] ),
+			PendingPlayerPid ! { start_game, game_pid },
+			io:fwrite( "HERE 23.0 ~n", [] ),
+			{reply, { start_game, game_pid }, NewState2};
+		_Other ->
+			io:fwrite( "HERE Other 22.0 ~p~n", [_Other] )
+	end.
+
+%%--------------------------------------------------------------------
+%% Function: handle_cast(Msg, State) -> {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, State}
+%% Description: Handling cast messages
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+	{noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_info(Info, State) -> {noreply, State} |
+%%                                       {noreply, State, Timeout} |
+%%                                       {stop, Reason, State}
+%% Description: Handling all non call/cast messages
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+	{noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: terminate(Reason, State) -> void()
+%% Description: This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any necessary
+%% cleaning up. When it returns, the gen_server terminates with Reason.
+%% The return value is ignored.
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+	ok.
+
+%%--------------------------------------------------------------------
+%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% Description: Convert process state when code is changed
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+	{ok, State}.
+
+%%--------------------------------------------------------------------
+%%% Internal functions
+%%--------------------------------------------------------------------
+
+get_pending_player( State ) ->
+	{ ok, PendingPlayer } = dict:find( pending_player, State ),
+	PendingPlayer.
+
+set_pending_player( State, PendingPlayer ) ->
+	dict:store( pending_player, PendingPlayer, State ).
